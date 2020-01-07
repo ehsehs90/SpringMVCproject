d793b7725ef54774d34b9b5b4e522f9cafbf1402
ehsehs <ehgus1551@naver.com>
2020-01-07 23:01:58 +0900

    200107| BoardService

--- src/main/java/com/springbook/biz/common/JDBCUtil.java
+++ src/main/java/com/springbook/biz/common/JDBCUtil.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class JDBCUtill {
+public class JDBCUtil {
 	public static Connection getConnection() {
 		try {
 			Class.forName("org.h2.Driver");
