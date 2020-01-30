6340bfede217912d7e782853b96bb3e734912d95
ehsehs <ehgus1551@naver.com>
2020-01-30 23:34:03 +0900

    200130 | 어노테이션으로 게시판 프로그램 구현하기

--- src/main/java/com/springbook/view/board/DeleteBoardController.java
+++ src/main/java/com/springbook/view/board/DeleteBoardController.java
@@ -1,33 +1,21 @@
 package com.springbook.view.board;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
-public class DeleteBoardController implements Controller{
+@Controller
+public class DeleteBoardController {
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("글 삭제 처리");
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
+		System.out.println("글 삭제 처리");		
+		boardDAO.deleteBoard(vo);		
+		return "getBoardList.do";
 		
-		//1. 사용자 입력정보 추출
-		String seq= request.getParameter("seq");
-		//2. DB연동
-		BoardVO vo = new BoardVO();
-		vo.setSeq(Integer.parseInt(seq));
 		
-		BoardDAO boardDAO = new BoardDAO();
-		boardDAO.deleteBoard(vo);
-		
-		//3. 화면 네비게이션
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("redirect:getBoardList.do");
-		return mav;
 	}
 
 }
--- src/main/java/com/springbook/view/board/InsertBoardController.java
+++ src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -1,7 +1,5 @@
 package com.springbook.view.board;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,29 +10,11 @@ import com.springbook.biz.board.impl.BoardDAO;
 public class InsertBoardController  {
 
 	@RequestMapping(value="/insertBoard.do")
-	public void insertBoard(HttpServletRequest request) {
+	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
 		System.out.println("글 등록 처리");
-		
-		//1. 사용자 입력 정보 추출
-		//request.setCharacterEncoding("UTF-8");
-		String title = request.getParameter("title");
-		String writer =	request.getParameter("writer");
-		String content = request.getParameter("content");
-		
-		//2. DB연동 처리
-		
-		BoardVO vo = new BoardVO();
-		vo.setTitle(title);
-		vo.setWriter(writer);
-		vo.setContent(content);
-		
-		BoardDAO boardDAO = new BoardDAO();
 		boardDAO.insertBoard(vo);
 		
-//		//3. 화면 네비게이션
-//		ModelAndView mav = new ModelAndView();
-//		mav.setViewName("redirect:getBoardList.do");
-//		return mav;
+		return "getBoardList.do";
 	}
 
 }
